16158b9d8eac8f71d940230da7e862657d18d0bb
CHISEN Kaoru <cordx56@cordx.net>
2023-03-23 07:04:15 +0900

    Update index.html

--- src/index_html.rs
+++ src/index_html.rs
@@ -10,42 +10,53 @@ pub fn generate_index_html(
         "Not Starlink - isstarlink.com"
     };
     let main_message = if is_starlink {
-        "You are connecting from Starlink network!"
+        r###"You are connecting from <span class="red">Starlink</span> network!"###
     } else {
-        "You are not connecting from Starlink network."
+        r###"You are <span class="red">NOT</span> connecting from Starlink network."###
     };
-    let query_addr_len = query_domain.len();
     let mut query_addrs = "".to_string();
     for domain in query_domain {
-        query_addrs = format!("{query_addrs}<td>{domain}</td>");
+        query_addrs = format!("{query_addrs}<li>{domain}</li>");
     }
     format!(
         r###"<!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="utf-8">
-            <title>{title}</title>
-          </head>
-          <body>
-            <h1>{main_message}</h1>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Query IP Address</th>
-                        <td>{query_addr}</td>
-                    </tr>
-                    <tr>
-                        <th>Remote IP Address</th>
-                        <td>{remote_addr}</td>
-                    </tr>
-                    <tr>
-                        <th rowspan="{query_addr_len}">Query Domains</th>
-                        {query_addrs}
-                    </tr>
-                </tbody>
-            </table>
-          </body>
-        </html>
-        "###
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>{title}</title>
+    <style>
+      .red {{
+        color: #ff0000;
+      }}
+    </style>
+  </head>
+  <body>
+    <h1>{main_message}</h1>
+    <p>
+      <a href="/json">You can use JSON format data.</a>
+    </p>
+    <table>
+        <tbody>
+            <tr>
+                <th>Query IP Address</th>
+                <td>{query_addr}</td>
+            </tr>
+            <tr>
+                <th>Remote IP Address</th>
+                <td>{remote_addr}</td>
+            </tr>
+            <tr>
+                <th>Query Domains</th>
+                <td>
+                  <ul>
+                    {query_addrs}
+                  </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+  </body>
+</html>
+"###
     )
 }
